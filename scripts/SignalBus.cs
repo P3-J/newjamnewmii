670772fa57458b653ce54ce7f97f178c4be38c19
Jako Keskküla <jako.keskkyla@gmail.com>
2024-11-09 18:47:42 +0200

    tester

--- scripts/SignalBus.cs
+++ scripts/SignalBus.cs
@@ -6,16 +6,4 @@ public partial class SignalBus : Node
 	[Signal]
     public delegate void PlayerTouchedCoinEventHandler(); // nimes peab olema EventHandler - stupid aga jahm
 
-
-    public override void _Ready()
-    {
-        
-        if (!playername = Jako){
-            return;
-        }
-        
-        asadasadasd
-    }
-
-
 }
