a5c64a528cc00b77839a10a3615674504730826d
Jako Keskküla <jako.keskkyla@gmail.com>
2024-11-17 14:40:52 +0200

    mainmenu

--- scripts/Mainmenu.cs
+++ scripts/Mainmenu.cs
@@ -3,7 +3,11 @@ using System;
 
 public partial class Mainmenu : Node2D
 {
+
+	private Globals globals;
 	public void _on_button_pressed(){
+	    globals = GetNode<Globals>("/root/Globals");
+		globals.currentFloor = 1;
 		string roomFile = "res://scenes/levels/room.tscn";
 		GetTree().ChangeSceneToFile(roomFile); 
 	}
